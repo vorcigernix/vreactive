95abd68611375a936419ebbc210fc8602666e4f0
Adam Sobotka <vorcigernix@gmail.com>
2019-03-03 19:22:57 +0000

    route

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ module.exports = {
     workboxPluginMode: "InjectManifest",
     workboxOptions: {
       swSrc: "src/service-worker.js",
+      navigateFallback: "/index.html",
       exclude: [/\.map$/, /manifest\.json$/]
     },
     themeColor: "#1da025"
