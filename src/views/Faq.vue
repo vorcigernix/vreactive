1229b331bd0c3bf83f642d4235c7901ce1c5474e
Adam Sobotka <sobotkaa@localhost.localdomain>
2019-03-07 15:15:01 +0100

    articlez

--- src/views/Faq.vue
+++ src/views/Faq.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="content">
     <h3>FAQ</h3>
+    <div v-html="compiledMarkdown"></div>
   </div>
 </template>
+<script>
+import marked from "marked";
+import faq from "!raw-loader!../../data/faq.md";
+export default {
+    computed: {
+    compiledMarkdown: function () {
+      return marked(faq, { sanitize: true })
+    }
+  },
+}; 
+
+</script>
--- src/views/Mentors.vue
+++ src/views/Mentors.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="content">
     <h3>Mentors</h3>
+    <div v-html="compiledMarkdown"></div>
   </div>
 </template>
+<script>
+import marked from "marked";
+import mentors from "!raw-loader!../../data/mentors.md";
+export default {
+    computed: {
+    compiledMarkdown: function () {
+      return marked(mentors, { sanitize: true })
+    }
+  },
+}; 
+
+</script>
