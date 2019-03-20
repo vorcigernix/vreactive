6a62917298cc6138a8c653507f481d94d24a1bb5
Adam Sobotka <sobotkaa@localhost.localdomain>
2019-03-20 18:17:54 +0100

    test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,9 @@
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    });
-    expect(wrapper.text()).toMatch(msg);
-  });
+import NavRouter from "@/components/NavRouter.vue";
+
+describe("NavRouter.vue", () => {
+  it('has a created hook', () => {
+    expect(typeof NavRouter.created).toBe('function')
+  })
+
 });
